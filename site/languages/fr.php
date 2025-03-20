34d713ba4d9aab12a186ab9c8ce72f4a69353620
Brice Braquin <teambbocho@gmail.com>
2025-03-20 17:49:17 +0100

    feat: added french to the languages

---
+++ site/languages/fr.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'code' => 'fr',
+    'default' => false,
+    'direction' => 'ltr',
+    'locale' => [
+        'LC_ALL' => 'fr_FR'
+    ],
+    'name' => 'Français',
+    'translations' => [
+        'blog.morearticles' => 'Plus d\'articles de blog',
+        'blog.next' => 'Suivant',
+        'blog.previous' => 'Précédent',
+        'blog.readmore' => 'En savoir plus',
+        'blog.title' => 'Derniers articles',
+        'blog.zuruck' => 'Retour à la page Blog',
+        'contact.consumeLess' => 'Consommer Moins',
+        'contact.contactAnalysis' => 'Contactez-nous pour une analyse de potentiel.',
+        'contact.jetztAnfragen' => 'Contactez Nous Maintenant',
+        'contact.produceMore' => 'Produire Plus',
+        'hero.efficiency' => 'Efficacité',
+        'faq.title' => 'FAQ - Questions fréquemment posées sur empAI'
+    ],
+    'url' => NULL
+];
